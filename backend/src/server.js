6e2c9d81dd6babb114eee1a94cab7f7c501a74c3
Tharun-2006161 <23A91A05F5@aec.edu.in>
2026-06-28 21:23:42 +0530

    Initialize Mongoose connection on server start

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,28 +3,34 @@ const app = require('./app');
 const config = require('./config');
 const logger = require('./utils/logger');
 
-const server = app.listen(config.port, () => {
-    logger.info(`Content Publishing API running on port ${config.port} in ${config.nodeEnv} mode`);
-    logger.info(`API docs: http://localhost:${config.port}/api-docs`);
-    
-    // Start background scheduler in main process to bypass Paid Worker tier
-    const { publishScheduledPosts } = require('./worker/scheduler');
-    setInterval(publishScheduledPosts, config.worker.intervalMs || 60000);
-    logger.info('In-process background scheduler started');
-});
+const { connectDB } = require('./config/database');
 
-// Graceful shutdown
-process.on('SIGTERM', () => {
-    logger.info('SIGTERM received, shutting down gracefully...');
-    server.close(() => {
-        logger.info('Server closed.');
-        process.exit(0);
+connectDB().then(() => {
+    const server = app.listen(config.port, () => {
+        logger.info(`Content Publishing API running on port ${config.port} in ${config.nodeEnv} mode`);
+        logger.info(`API docs: http://localhost:${config.port}/api-docs`);
+        
+        // Start background scheduler in main process to bypass Paid Worker tier
+        const { publishScheduledPosts } = require('./worker/scheduler');
+        setInterval(publishScheduledPosts, config.worker.intervalMs || 60000);
+        logger.info('In-process background scheduler started');
     });
+
+    // Graceful shutdown
+    process.on('SIGTERM', () => {
+        logger.info('SIGTERM received, shutting down gracefully...');
+        server.close(() => {
+            logger.info('Server closed.');
+            process.exit(0);
+        });
+    });
+
+    process.on('SIGINT', () => {
+        logger.info('SIGINT received, shutting down...');
+        server.close(() => process.exit(0));
+    });
+
+    module.exports = server;
 });
 
-process.on('SIGINT', () => {
-    logger.info('SIGINT received, shutting down...');
-    server.close(() => process.exit(0));
-});
 
-module.exports = server;
